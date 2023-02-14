57217ea21437faa7265817698edf115ca4e49cc8
Anderson Reis <andersonreis1234555@gmail.com>
2023-02-14 10:16:06 -0300

    Finalizado

--- MediaEscolar/Media.js
+++ MediaEscolar/Media.js
@@ -11,7 +11,7 @@ let status = true
 
 
 
-function mediaEscolar(nota1, nota2, nota3, nota4) {
+function mediaEscolar(nota1, nota2, nota3, nota4, notadoExame) {
 
     let resultado
     let resultadoSomadoComExame
@@ -19,61 +19,24 @@ function mediaEscolar(nota1, nota2, nota3, nota4) {
     let nota2DadosDoAluno = Number(nota2)
     let nota3DadosDoAluno = Number(nota3)
     let nota4DadosDoAluno = Number(nota4)
-    let notaDoExameDados = notadoExame
+    let notaDoExameDados = Number(notadoExame)
 
 
+    resultado = (nota1DadosDoAluno + nota2DadosDoAluno + nota3DadosDoAluno + nota4DadosDoAluno) / 4
+    resultadoSomadoComExame = (nota1DadosDoAluno + nota2DadosDoAluno + nota3DadosDoAluno + nota4DadosDoAluno + exameEscolar(notaDoExameDados)) / 5
 
 
-    if (isNaN(nota1DadosDoAluno) || isNaN(nota2DadosDoAluno) || isNaN(nota3DadosDoAluno) || isNaN(nota4DadosDoAluno || isNaN(notaDoExameDados))) {
-        console.log('Erro: insiriu número')
-       return status = false
-    } else if (nota1DadosDoAluno === '' || nota2DadosDoAluno === '' || nota3DadosDoAluno === '' || nota4DadosDoAluno === '' || notaDoExameDados === '') {
-        console.log('Erro: insiriu vázio')
-        return status = false
-    } else if (nota1DadosDoAluno < 0 || nota1DadosDoAluno > 100 || nota2DadosDoAluno < 0 || nota2DadosDoAluno > 100 || nota3DadosDoAluno < 0 || nota3DadosDoAluno > 100 || nota4DadosDoAluno < 0 || nota4DadosDoAluno > 100 || notaDoExameDados < 0 || notaDoExameDados > 100) {
-        console.log('Erro: nota maior ou menor do que o permitido')
-        return status = false
+    if (resultado >= 70 && resultado <= 100) {
+        return resultado
+    } else if (resultado >= 50 && resultado <= 69) {
+        return resultadoSomadoComExame
     } else {
-        resultado = (nota1DadosDoAluno + nota2DadosDoAluno + nota3DadosDoAluno + nota4DadosDoAluno) / 4
-        resultadoSomadoComExame = (resultado + exameEscolar(notaDoExameDados)) / 5
-        console.log(resultado)
-        console.log(resultadoSomadoComExame)
-
-        if (resultado >= 70 && resultado <= 100) {
-
-            return 'Aprovado'
-            
-        } else if (resultado >= 50 && resultado <= 69) {
-          
-            
-            if (resultadoSomadoComExame < 0 || resultadoSomadoComExame > 100) {
-                return "Você digitou um número maior ou menor que o desejavel"
-            }
-             else if( resultadoSomadoComExame >= 70 && resultadoSomadoComExame <= 100) {
-          
-                return "Aprovado"
-                
-            }else if(resultadoSomadoComExame >= 50 && resultadoSomadoComExame <= 69){
-                return "reprovado"
-                
-            }
-
-        } else {
-            return "Erro"
-          
-        }
-
-
+        return resultado
     }
 
+
+
 }
-
-
-
-
-
-
-
 
 
 
@@ -82,111 +45,60 @@ function exameEscolar(notaDoExame) {
 
     let resultadoDoExame = notaDoExame
 
+    return resultadoDoExame
 
 
-    if (resultadoDoExame < 0 || resultadoDoExame > 100) {
-        return "ERRO: Você digitou um número maior ou menor que o desejavel"
-    }
-    else{
-        return resultadoDoExame
-    }
-
-
-    
-       
-        }
-   
+}
 
 
 
-
-
-function escolherGeneroAluno(generoAluno){
-    let strAluno 
+function escolherGeneroAluno(generoAluno) {
+    let strAluno
     let generoDadosAluno = generoAluno
-    
-    
-    if (  generoDadosAluno == "feminino") {
+
+
+    if (generoDadosAluno == "feminino") {
         strAluno = "A Aluna "
         return strAluno
-        
-    }  else if (generoDadosAluno == "masculino") {
-         strAluno = "O Aluno "
-         return strAluno
-    }  else {
-        console.log("Erro: de genero")
+
+    } else if (generoDadosAluno == "masculino") {
+        strAluno = "O Aluno "
+        return strAluno
+    } else {
+        return status = false
     }
 
 }
 
 
 
-function escolherGeneroProfessor(generoProfessor){
-    let strProfessor 
+function escolherGeneroProfessor(generoProfessor) {
+    let strProfessor
     let generoDadosProfessor = generoProfessor
-    
-    if(!String(generoDadosProfessor) ){
-    
-        console.log("false")
-    }
-    else if (""){
-    }
-    else if (  generoDadosProfessor == "feminino") {
+
+
+    if (generoDadosProfessor == "feminino") {
         strProfessor = "A Professora "
         return strProfessor
-        
-    }  else if (generoDadosProfessor == "masculino") {
+
+    } else if (generoDadosProfessor == "masculino") {
         strProfessor = "O Professor "
         return strProfessor
 
-    }  else {
-      return   strProfessor
-    }
-
-}
-
-
-
-
-function dadosPessoais(nomeAluno, curso, nomeProfessor, generoAluno, generoProfessor, disciplina) {
-
-    let nomeAlunoDados = nomeAluno
-    let cursoDados = curso
-    let nomeProfessorDados = nomeProfessor
-    let generoDadosAluno = generoAluno
-    let generoDadosProfessor = generoProfessor
-    let disciplinaDados = disciplina
-   
-
-
-    if (!String(nomeAlunoDados) || !String(cursoDados) || !String(nomeAlunoDados) || !String(generoDadosAluno)   || !String(disciplinaDados) || !String(generoDadosProfessor)) {
-
-
-        console.log('Você está digitando caracteres errado')
-
-        status = false
-    } else if (nomeAlunoDados == '' || cursoDados == '' || nomeProfessorDados == '' || generoDadosAluno == ''  || disciplinaDados == '' || generoDadosProfessor == '') {
-
-
-        console.log('Você está digitando caracteres errado')
-        status = false
-
     } else {
-        console.log(escolherGeneroAluno(generoDadosAluno) + " " + nomeAlunoDados + " teve o resultado: " + mediaEscolar(80,80,80,60,30) + " na disciplina " + disciplinaDados)
-       
-
-
+        return status = false
     }
 
 }
+
 
 module.exports = { // deixando a função pública
-    dadosPessoais,
     escolherGeneroAluno,
     escolherGeneroProfessor,
     mediaEscolar,
     exameEscolar
-    
+
+
 }
 
 
--- MediaEscolar/app.js
+++ MediaEscolar/app.js
@@ -5,79 +5,145 @@ var entraDados = readline.createInterface({ // entrada de dados
     output: process.stdout
 
 });
-entraDados.question('Digite o nome do aluno: ', function (nome) {
-let nomeALuno = nome
+entraDados.question('Digite o nome do aluno: \n', function (nome) {
+    let nomeALuno = nome
 
-entraDados.question('Digite o nome do professor: ', function (nome) {
-    let nomeProfessor = nome
-    
-    entraDados.question('Digite o sexo do aluno', function (sexo) {
+    entraDados.question('Digite o nome do professor: \n', function (nome) {
+        let nomeProfessor = nome
 
-        let generoAluno = sexo
-        
-        entraDados.question("Digite o sexo do professor", function (sexo) {
+        entraDados.question('Digite o sexo do aluno: \n', function (sexo) {
 
-           let generoProfessor = sexo
-           
-           entraDados.question('Digite o nome do curso', function (curso) {
+            let generoAluno = sexo
 
-            let nomeCurso = curso
-            
-            entraDados.question("Digite o nome da disciplina", function (disciplina) {
+            entraDados.question("Digite o sexo do professor: \n", function (sexo) {
 
-                let nomeDisciplina = disciplina
-                
+                let generoProfessor = sexo
 
-                entraDados.question("Insira sua primeira nota", function(nota1){
-                    nota1Dados = nota1
+                entraDados.question('Digite o nome do curso: \n', function (curso) {
 
-                    entraDados.question("Insira sua primeira nota", function(nota2){
-                        nota2Dados = nota2
+                    let nomeCurso = curso
 
-                        entraDados.question("Insira sua primeira nota", function(nota3){
-                            nota3Dados = nota3
+                    entraDados.question("Digite o nome da disciplina: \n", function (disciplina) {
 
-                            entraDados.question("Insira sua primeira nota", function(nota4){
-                                nota4Dados = nota4
+                        let nomeDisciplina = disciplina
 
-                             if(nomeALuno == '' || nomeProfessor == '' || generoAluno == '' || generoProfessor == ''){
-                                console.log("Erro: você não colocou todos os dados")
-                             }else if(nota1Dados == '' || nota2Dados == '' || nota3Dados == '' || nota4Dados ==  ''){
-                                console.log("Erro: você não colocou todos os dados")
-                             }else if(nota1Dados == isNaN || nota2Dados == isNaN || nota3Dados == isNaN || nota4Dados ==  isNaN){
-                                console.log("Erro: não é um número")
-                             }
-                             else {
 
-                              let resultadoFinal = media.mediaEscolar(nota1Dados, nota2Dados, nota3Dados, nota4Dados, 0)
-                              if(resultadoFinal() >=50 || resultadoFinal()  <=69){
-                                console.log("Você caiu na recuperação")
-                                entraDados.question("Digite a nota do seu exame", function(exame){
-                                    let resultadoFinalComExame = media.mediaEscolar(nota1Dados, nota2Dados, nota3Dados, nota4Dados, notaExame)
-                                    if(resultadoFinalComExame >= 69 || resultadoFinalComExame<=100){
-                                        console.log("Aprovado")
-                                    }else {
-                                        console.log("Reprovado")
-                                    }
-                                   
+                        entraDados.question("Insira sua primeira nota: \n", function (nota1) {
+                            let nota1Dados = nota1
+
+                            entraDados.question("Insira sua segunda nota: \n", function (nota2) {
+                                let nota2Dados = nota2
+
+                                entraDados.question("Insira sua terceira nota: \n", function (nota3) {
+                                    let nota3Dados = nota3
+
+                                    entraDados.question("Insira sua quarta nota: \n", function (nota4) {
+                                        let nota4Dados = nota4
+
+                                        if (nomeALuno == '' || nomeProfessor == '' || generoAluno == '' || generoProfessor == '' || nomeCurso == '' || nomeDisciplina == '') {
+                                            console.log("Erro: você não colocou todos os dados")
+                                        } else if (nota1Dados == '' || nota2Dados == '' || nota3Dados == '' || nota4Dados == '') {
+                                            console.log("Erro: você não colocou todos os dados")
+                                        } else if (isNaN(nota1Dados) || isNaN(nota2Dados) || isNaN(nota3Dados) || isNaN(nota4Dados)) {
+                                            console.log("Erro: não é um número")
+                                        }
+                                        else if (nota1Dados < 0 || nota1Dados > 100 || nota2Dados < 0 || nota2Dados > 100 || nota3Dados < 0 || nota3Dados > 100 || nota4Dados < 0 || nota4Dados > 100) {
+                                            console.log("Erro número menor ou maior do que o permitido")
+                                        }
+                                        else {
+
+
+                                            let resultadoFinal = media.mediaEscolar(nota1Dados, nota2Dados, nota3Dados, nota4Dados, 0)
+
+
+                                            if (resultadoFinal >= 50 && resultadoFinal <= 69) {
+
+                                                console.log("Você caiu na recuperação")
+                                                console.log(resultadoFinal)
+
+                                                entraDados.question("Digite a nota do seu exame: \n", function (exame) {
+                                                    let notaExame = exame
+                                                    let resultadoFinalComExame = media.mediaEscolar(nota1Dados, nota2Dados, nota3Dados, nota4Dados, notaExame)
+                                                    console.log(resultadoFinalComExame)
+                                                    if (notaExame = '') {
+                                                        console.log("Erro: Algum dado fornecido está vázio")
+                                                    }
+                                                    else if (isNaN(notaExame)) {
+                                                        console.log("Erro: você digitou caracteres errado")
+                                                    }
+                                                    else if (notaExame < 0 || notaExame > 100) {
+                                                        console.log("Erro número menor ou maior do que o permitido")
+                                                    } else {
+
+                                                        if (resultadoFinalComExame >= 70 && resultadoFinalComExame <= 100) {
+                                                            console.log(media.escolherGeneroAluno(generoAluno) + " " + nomeALuno + " foi " + "Aprovado" + " na disciplina " + nomeDisciplina)
+                                                            console.log("Curso: " + nomeCurso);
+                                                            console.log("Professor: " + nomeProfessor)
+                                                            console.log("Nota do aluno: nota1: " + nota1Dados + " nota2: " + nota2Dados + "nota3: " + nota3Dados + " nota4: " + nota4Dados + "Exame: " + media.exameEscolar(notaExame))
+                                                            console.log("Média final: " + resultadoFinal);
+                                                            console.log("Média final do exame " + resultadoFinalComExame)
+                                                        }
+                                                        else if (resultadoFinalComExame >= 50 && resultadoFinalComExame <= 69) {
+                                                            console.log(media.escolherGeneroAluno(generoAluno) + " " + nomeALuno + " foi " + "Aprovado" + " na disciplina " + nomeDisciplina)
+                                                            console.log("Curso: " + nomeCurso);
+                                                            console.log("Professor: " + nomeProfessor)
+                                                            console.log("Nota do aluno: nota1: " + nota1Dados + " nota2: " + nota2Dados + "nota3: " + nota3Dados + " nota4: " + nota4Dados + "Exame: " + media.exameEscolar(notaExame))
+                                                            console.log("Média final: " + resultadoFinal);
+                                                            console.log("Média final do exame " + resultadoFinalComExame)
+                                                        } else if (resultadoFinalComExame >= 0 && resultadoFinalComExame <= 49) {
+                                                            console.log(media.escolherGeneroAluno(generoAluno) + " " + nomeALuno + " foi " + "Reprovado" + " na disciplina " + nomeDisciplina)
+                                                            console.log("Curso: " + nomeCurso);
+                                                            console.log("Professor: " + nomeProfessor)
+                                                            console.log("Nota do aluno: nota1: " + nota1Dados + " nota2: " + nota2Dados + "nota3: " + nota3Dados + " nota4: " + nota4Dados + "Exame: " + media.exameEscolar(notaExame))
+                                                            console.log("Média final: " + resultadoFinal);
+                                                            console.log("Média final do exame " + resultadoFinalComExame)
+                                                        } else {
+                                                            if (resultadoFinalComExame == false) {
+                                                                entraDados.close
+                                                            }
+                                                        }
+
+                                                    }
+
+
+                                                })
+                                            }
+                                            else if (resultadoFinal >= 70 && resultadoFinal <= 100) {
+                                                console.log(media.escolherGeneroAluno(generoAluno) + " " + nomeALuno + " foi " + "Aprovado" + " na disciplina " + nomeDisciplina)
+                                                console.log("Curso: " + nomeCurso);
+                                                console.log("Professor: " + nomeProfessor)
+                                                console.log("Nota do aluno: nota1: " + nota1Dados + " nota2: " + nota2Dados + "nota3: " + nota3Dados + " nota4: " + nota4Dados)
+                                                console.log("Média final: " + resultadoFinal);
+
+                                            }
+                                            else if (resultadoFinal >= 0 && resultadoFinal <= 49) {
+                                                console.log(media.escolherGeneroAluno(generoAluno) + " " + nomeALuno + " foi " + "Reprovado" + " na disciplina " + nomeDisciplina)
+                                                console.log("Curso: " + nomeCurso);
+                                                console.log("Professor: " + nomeProfessor)
+                                                console.log("Nota do aluno: nota1: " + nota1Dados + " nota2: " + nota2Dados + " nota3: " + nota3Dados + " nota4: " + nota4Dados)
+                                                console.log("Média final: " + resultadoFinal);
+
+                                            }
+
+                                            else {
+                                                if (resultadoFinal == false) {
+                                                    entraDados.close
+                                                }
+                                            }
+                                        }
+
+                                    })
+
+
                                 })
-                              }else{
-                                  if(resultadoFinal == false){
-                                      entraDados.close
-                                  }
-                              }
-                            }
-                                
-
                             })
                         })
                     })
+
+
                 })
-            
-        
             })
-           })
         })
     })
 })
-})
+
